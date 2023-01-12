a9440e2c21dd3f6d1881808d1c6170b5f0dceb5a
VanLuan123 <105442986+VanLuan123@users.noreply.github.com>
2023-01-12 15:58:30 +0700

    Update sign out

--- app/src/main/java/com/example/doannhom11/MainActivity2.java
+++ app/src/main/java/com/example/doannhom11/MainActivity2.java
@@ -8,9 +8,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,12 +27,38 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MainActivity2 extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-
+    ImageView btnSignOut;
     DocumentReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //Đăng xuất
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+                builder.setIcon(R.drawable.ic_baseline_exit_to_app_blue);
+                builder.setTitle("Đăng xuất");
+                builder.setMessage("Bạn muốn đăng xuất?");
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        mAuth.signOut();
+                        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance().document("CUAHANG/" + mAuth.getUid());
         if(mAuth.getCurrentUser() == null){
