3dd8ba1ed21bf3f8a4ab124fb20cc64737e8a5b9
Kien <20521487@gm.uit.edu.vn>
2023-01-28 20:20:05 +0700

    them adapter

--- app/src/main/java/com/example/doannhom11/Drinks.java
+++ app/src/main/java/com/example/doannhom11/Drinks.java
@@ -1,5 +1,13 @@
 package com.example.doannhom11;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
 public class Drinks {
     private String name,size,topping;
     private int ban;
@@ -33,5 +41,37 @@ public class Drinks {
     }
     public void setBan(int ban) {
         this.ban = ban;
+    }
+}
+class DrinksAdapter extends BaseAdapter
+{
+    private Context m_Context;
+    private ArrayList<Drinks> m_array;
+    private int m_Layout;
+    public DrinksAdapter (Context m_Context,int m_Layout, ArrayList<Drinks> m_array)
+    {
+        this.m_Context=m_Context;
+        this.m_Layout=m_Layout;
+        this.m_array=m_array;
+    }
+    @Override
+    public int getCount() {
+        return m_array.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = LayoutInflater.from(m_Context).inflate(m_Layout,null);
+        return null;
     }
 }
--- app/src/main/java/com/example/doannhom11/Product.java
+++ app/src/main/java/com/example/doannhom11/Product.java
@@ -1,11 +1,21 @@
 package com.example.doannhom11;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
 public class Product {
     private String masp,tensp;
     private int gia;
 
     public Product() {
-
     }
 
     public Product(String masp, String tensp, int gia) {
@@ -35,5 +45,39 @@ public class Product {
 
     public void setGia(int gia) {
         this.gia = gia;
+    }
+}
+class ProductAdapter extends BaseAdapter
+{
+    TextView tvname,tvmasp,tvprice;
+    ImageView ava;
+    private Context m_Context;
+    private ArrayList<Product> m_array,temp_array;
+    private int m_Layout;
+    public ProductAdapter(Context context, int layout, ArrayList<Product> arrayList)
+    {
+        m_Context = context;
+        m_Layout = layout;
+        m_array = arrayList;
+        temp_array = arrayList;
+    }
+    @Override
+    public int getCount() {
+        return m_array.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return m_array.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = LayoutInflater.from(m_Context).inflate(m_Layout,null);
+        return view;
     }
 }
