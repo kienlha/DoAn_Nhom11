e1886eee369f3fe51d45e69d57dd49ee5bf3daa8
Kien <20521487@gm.uit.edu.vn>
2023-02-01 22:01:09 +0700

    test table

--- app/src/main/java/com/example/doannhom11/Fragment_table.java
+++ app/src/main/java/com/example/doannhom11/Fragment_table.java
@@ -1,12 +1,47 @@
 package com.example.doannhom11;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.view.menu.MenuPopupHelper;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +90,140 @@ public class Fragment_table extends Fragment {
         }
     }
 
+    GridView tableList;
+    ArrayList<MyBool> table;
+    TableAdapter adapter;
+    MenuBuilder menuBuilder;
+    DocumentReference db;
+    FirebaseAuth mAuth;
+
+    ImageView addImg;
+    SwipeRefreshLayout refreshLayout;
+
+    @SuppressLint("RestrictedApi")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_table, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_table, container, false);
+        tableList = (GridView) view.findViewById(R.id.tableList);
+        addImg = (ImageView)view.findViewById(R.id.btnAddTable);
+        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeLayoutTable);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance().document("CUAHANG/" + mAuth.getUid());
+
+        addImg.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(getContext());
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.layout_table_add);
+
+                Window window = dialog.getWindow();
+                window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                if(window == null){
+                    return;
+                }
+
+                ((Button)dialog.findViewById(R.id.btnCancel)).setOnClickListener(view1 ->  {
+                    dialog.dismiss();
+                });
+
+                ((Button)dialog.findViewById(R.id.btnAdd)).setOnClickListener(view2 -> {
+                    String s = ((EditText)dialog.findViewById(R.id.edtTableIndex)).getText().toString();
+                    if (!s.isEmpty())
+                    {
+                        int index = Integer.parseInt(s);
+                        db.collection("TableStatus").whereEqualTo("Index", index).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful())
+                                {
+                                    for (DocumentSnapshot data : task.getResult())
+                                    {
+                                        CustomToast.e(getActivity(), "Bàn số " + index + " đã có sẵn", Toast.LENGTH_SHORT);
+                                        return;
+                                    }
+                                    Map<String, Object> map = new HashMap<>();
+                                    map.put("Index", index);
+                                    db.collection("TableStatus").add(map).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                                            Map<String, Object> map = new HashMap<>();
+                                            map.put("status", false);
+                                            db.collection("/TableStatus/").document(task.getResult().getId()).update(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    CreateList();
+                                                }
+                                            });
+                                        }
+                                    });
+                                    dialog.dismiss();
+                                }
+                                else
+                                    CustomToast.e(getActivity(), task.getException().getMessage(), Toast.LENGTH_LONG);
+                            }
+                        });
+                    }
+                    else
+                        CustomToast.e(getActivity(), "Vui lòng nhập số bàn!", Toast.LENGTH_SHORT);
+                });
+
+                dialog.show();
+
+                CreateList();
+            }
+        });
+
+
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                CreateList();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
+        CreateList();
+
+        return view;
+    }
+
+    private void CreateList() {
+
+        db.collection("/TableStatus").orderBy("Index", Query.Direction.ASCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    table = new ArrayList<>();
+                    adapter = new TableAdapter(getContext(), table);
+                    tableList.setAdapter(adapter);
+                    for (QueryDocumentSnapshot data : task.getResult()) {
+                        MyBool val = new MyBool(data.getId(), false, Integer.parseInt(data.getLong("Index")+""));
+                        table.add(val);
+
+                        db.collection("TableStatus/" + data.getId() + "/DrinksOrder").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task1) {
+                                for (DocumentSnapshot data : task1.getResult())
+                                {
+                                    val.Set(true);
+                                    adapter.notifyDataSetChanged();
+                                    break;
+                                }
+
+                            }
+                        });
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
     }
 }
