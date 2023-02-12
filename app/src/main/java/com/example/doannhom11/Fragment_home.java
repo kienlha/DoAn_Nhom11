16461b8481c9f7ef422a63bb0b8233f7150983c3
Kien <20521487@gm.uit.edu.vn>
2023-02-12 21:17:46 +0700

    home

--- app/src/main/java/com/example/doannhom11/Fragment_home.java
+++ app/src/main/java/com/example/doannhom11/Fragment_home.java
@@ -3,10 +3,18 @@ package com.example.doannhom11;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +63,29 @@ public class Fragment_home extends Fragment {
         }
     }
 
+    ListView listDrinks;
+    SwipeRefreshLayout refreshLayout;
+    DocumentReference db;
+    FirebaseAuth mAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        listDrinks = (ListView) view.findViewById(R.id.lvDrinkStack);
+        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeLayoutHome);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance().document("CUAHANG/" + mAuth.getUid());
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshLayout.setRefreshing(false);
+            }
+        });
+        return view;
     }
 }
