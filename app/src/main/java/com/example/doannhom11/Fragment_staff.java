6cba2af9607c1ab11f2c9479178778c9428636c3
VanLuan123 <105442986+VanLuan123@users.noreply.github.com>
2023-02-14 15:42:18 +0700

    Update Fragment_staff.java

--- app/src/main/java/com/example/doannhom11/Fragment_staff.java
+++ app/src/main/java/com/example/doannhom11/Fragment_staff.java
@@ -1,12 +1,27 @@
 package com.example.doannhom11;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,12 +68,62 @@ public class Fragment_staff extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
 
+    }
+    ImageView btnAddStaff;
+    ArrayList<Staff> staffs;
+    StaffAdapter adapter;
+    ListView listView ;
+    FirebaseAuth mAuth;
+    DocumentReference db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_staff, container, false);
+        View root = inflater.inflate(R.layout.fragment_staff, container, false);
+
+        listView = (ListView) root.findViewById(R.id.ListOfStaffs);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance().document("CUAHANG/" + mAuth.getUid());
+
+        db.collection("/NHANVIEN").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    staffs = new ArrayList<>();
+                    for (QueryDocumentSnapshot data : task.getResult())
+                    {
+                        String CCCD = data.getString("CCCD"),
+                                HOTEN = data.getString("HOTEN"),
+                                NGAYSINH = data.getString("NGAYSINH"),
+                                GIOITINH = data.getString("GIOITINH"),
+                                SDT = data.getString("SDT"),
+                                NGVL = data.getString("NGVL"),
+                                CHUCVU = data.getString("CHUCVU"),
+                                MANV = data.getId(),
+                                Email = data.getString("EMAIL");
+
+                        staffs.add(new Staff(MANV, HOTEN, NGAYSINH, GIOITINH ,SDT, NGVL, CHUCVU, CCCD, Email ));
+                    }
+                    adapter = new StaffAdapter(getActivity(), R.layout.layout_staff_manage, staffs);
+                    listView.setAdapter(adapter);
+
+                }
+            }
+        });
+
+
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("MANV", staffs.get(i).MaNhanVien());
+            Navigation.findNavController(view).navigate(R.id.action_fragment_staff_to_fragment_staff_info, bundle);
+        });
+
+        ((ImageView)root.findViewById(R.id.btnAddInfo)).setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_fragment_staff_to_fragment_staff_edit);
+        });
+
+        return root;
     }
 }
